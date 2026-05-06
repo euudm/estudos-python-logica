b2b730af576c36d64e4bf4ca877cc46c1f63400a
Diego de Moraes Moura <diegodmoraes123@gmail.com>
2026-05-06 08:21:21 -0300

    adicionando um exercios de logica

---
+++ python/codigos_teste.py
@@ -0,0 +1,14 @@
+# Definindo o prazo total
+prazo_total = 24
+
+# Calculando entregas de cada time
+entregas_time_a = prazo_total / 4
+entregas_time_b = prazo_total / 6
+entregas_time_c = prazo_total / 8
+
+# Somando o total
+total_projetos = entregas_time_a + entregas_time_b + entregas_time_c
+
+print(total_projetos)
+
+#objetivo é saber quantos projetos cada time entragria juntos em 24 dias, sabendo que o time A entrega um projeto a cada 4 dias, o time B entrega um projeto a cada 6 dias e o time C entrega um projeto a cada 8 dias.
